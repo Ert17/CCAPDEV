5cf8b6348820efb5a3458c26047a4a015324b83e
Earth <earth_wendell_lopez@dlsu.edu.ph>
2020-04-24 18:13:03 +0800

    UPDATE

--- controllers/controller.js
+++ controllers/controller.js
@@ -31,7 +31,7 @@ const controller = {
                 res.render('browse', 
                     {
                         query: req.params.query,
-                        items: results.map(results => results.toJSON())
+                        items: results
                     });
             }
         });
@@ -48,51 +48,54 @@ const controller = {
         var projection = 'iName seller price quantity bio MOD meet_location contact photo';
 
         db.findOne(Item, query, projection, function (result) {
+            db.findMany(Review, query, '', function (results) {
+                
+                if(result != null) {
+                    var details = {
+                        iName: result.iName,    
+                        seller: result.seller,
+                        price: result.price,
+                        quantity: result.quantity,
+                        bio: result.bio,
+                        MOD: result.MOD,
+                        meet_location: result.meet_location,
+                        contact: result.contact,
+                        photo: result.photo,
+                        reviews: results
+                    };
 
-            if(result != null) {
-                var details = {
-                    iName: result.iName,    
-                    seller: result.seller,
-                    price: result.price,
-                    quantity: result.quantity,
-                    bio: result.bio,
-                    MOD: result.MOD,
-                    meet_location: result.meet_location,
-                    contact: result.contact,
-                    photo: result.photo,
-                };
-
-                res.render('item', details);
-            }
+                    res.render('item', details);
+                }
+            });
         });
 	},
 
     getUser: function (req, res) {
 
-        var query = {username: req.params.username};
+        var query1 = {username: req.params.username};
+        var query2 = {seller: req.params.username};
 
-        var projection = 'fName lName username bio photo';
+        // var projection = 'fName lName username bio photo';
 
-        db.findOne(User, query, projection, function (result) {
-            // db.findMany(Item, query, projection, function (Iresult) {
-            //     res.render('profile',
-            //         {
-            //             username: req.params.username,
+        db.findOne(User, query1, '', function (result) {
+            db.findMany(Item, query2, '', function (results1) {
+                db.findMany(Review, query2, '', function (results2) {
 
-            //         }
-            // })
+                    if(result != null) {
+                        var details = {
+                            fName: result.fName,    
+                            lName: result.lName,
+                            username: result.username,
+                            bio: result.bio,
+                            photo: result.photo,
+                            items: results1,
+                            reviews: results2
+                        };
 
-            if(result != null) {
-                var details = {
-                    fName: result.fName,    
-                    lName: result.lName,
-                    username: result.username,
-                    bio: result.bio,
-                    photo: result.photo
-                };
-
-                res.render('profile', details);
-            }
+                        res.render('profile', details);
+                    }
+                });
+            });   
         });
     }
 }
