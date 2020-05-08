7a57d281f5425b3b19264d04899e92afbbb8a881
Earth <earth_wendell_lopez@dlsu.edu.ph>
2020-05-08 19:18:34 +0800

    Fixed Image Preview and update feature

--- public/js/complete.js
+++ public/js/complete.js
@@ -2,14 +2,44 @@ function readURL(input) {
 	if (input.files && input.files[0]) {
 		var reader = new FileReader();
 		reader.onload = function(e) {
-			$('#imagePreview').css('background-image', 'url('+e.target.result +')');
-			$('#imagePreview').hide();
-			$('#imagePreview').fadeIn(650);
+			$('.imagePreview').css('background-image', 'url('+e.target.result +')');
+			$('.imagePreview').hide();
+			$('.imagePreview').fadeIn(650);
 		}
 		reader.readAsDataURL(input.files[0]);
 	}
 }
 $("#imageUpload").change(function() {
+	readURL(this);
+});
+
+function readURL2(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$('.imagePreview2').css('background-image', 'url('+e.target.result +')');
+			$('.imagePreview2').hide();
+			$('.imagePreview2').fadeIn(650);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+$("#photoUpload").change(function() {
+	readURL(this);
+});
+
+function readURL3(input) {
+	if (input.files && input.files[0]) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$('.imagePreview3').css('background-image', 'url('+e.target.result +')');
+			$('.imagePreview3').hide();
+			$('.imagePreview3').fadeIn(650);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+}
+$("#imageUpload2").change(function() {
 	readURL(this);
 });
 
